9568ceb8ea6ae433763c85a3bd5c4f74da1d9575
leora-kowen <leorak513@gmail.com>
2021-02-13 19:07:55 +0200

    Outline for Song and Note classes

--- leora_and_gabi_project/abstract_piano.js
+++ leora_and_gabi_project/abstract_piano.js
@@ -36,12 +36,60 @@ const createPath = (top) => {
   }, 5);
 };
 
+class Note {
+  constructor(note, length, octave, rate) {
+    this.note_char = note;
+    this.length = length;
+    this.octave = octave;
+    this.getNoteSamples(rate);
+  }
+
+  getNoteSamples(rate) {
+    /*
+     * Use note_char, length, octave  and rate to generate/create/import
+     * "this.note_sample" and "this.wrong_note_sample" (quarter tone
+     * higher/lower)
+     */
+  }
+
+  playNote() {
+    /* play(this.note_sample)*/
+  }
+
+  playWrongNote() {
+    /* play(this.wrong_note_sample)*/
+  }
+}
+
+class Song {
+  constructor(rate, notes) {
+    this.rate = rate;
+    this.time_frame = [];
+    for (var note in notes) {
+      if (note == null) this.time_frame.add(null);
+      this.time_frame.add(
+        new Note(note.note, note.length.note.octave, this.rate)
+      );
+    }
+  }
+}
+
 const main = () => {
   /* Demo for path creation */
   view.onClick = (event) => {
     var top = new Point(event.point.x, 0);
     createPath(top);
   };
+
+  /* Demo for "Piano" */
+  /*
+  * First objective: Implement the methods of "class Note", and create simple
+  * "piano" - Using keys A,S,D,F,H,J,K,L play notes C,D,E,F,G,A,B,C (octave 1
+  * for the first seven notes, and octave 2 for the last "C" note), at rate 60 BPM
+  * and length 1 beat.
+  */
+
+
 };
 
 main();
