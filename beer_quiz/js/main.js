dd8758237297348c25ccbf5f1b05ab97d70e046e
“manofsun” <omrisamson1@mail.tau.ac.il>
2021-03-01 20:12:02 +0200

    Omri Samson's files

--- beer_quiz/js/main.js
+++ beer_quiz/js/main.js
@@ -1,5 +1,6 @@
-const startButton = document.getElementById("start-btn");
 import verification from "./modules/ageVerification.js";
+const startButton = document.getElementById("start-btn");
+
 
 
 const player = new Tone.Player("/beer_quiz/music/pubambience.mp3").toDestination();
