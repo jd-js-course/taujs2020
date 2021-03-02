441816d30eba7efbc63a6a49d632128da17bc2c2
leora-kowen <leorak513@gmail.com>
2021-03-02 17:10:35 +0200

    Adding color schemes for each song, final game tweaks and code documentation

--- leora_and_gabi_project/abstract_piano.js
+++ leora_and_gabi_project/abstract_piano.js
@@ -1,20 +1,7 @@
-const note_to_color = {
-  "C": "#20134d",
-  "D": "#76a5af",
-  "E": "#8988ad",
-  "F": "#d9a944",
-  "G": "#bc706b",
-  "A": "#8a2813",
-  "B": "#b45d1a",
-  "C#": "#20134d",
-  "D#": "#76a5af",
-  "E#": "#8988ad",
-  "F#": "#d9a944",
-  "G#": "#bc706b",
-  "A#": "#8a2813",
-  "B#": "#b45d1a"
-}
+// Global app instance for the game itself
+var app = null;
 
+// Index from 0 to 6. Used for the target of the path.
 const note_to_position = {
   "C": 0,
   "D": 1,
@@ -32,6 +19,7 @@ const note_to_position = {
   "B#": 6
 }
 
+// Keyboard key for each musical note
 const note_to_key = {
   "C": "A",
   "D": "S",
@@ -49,10 +37,11 @@ const note_to_key = {
   "B#": "J"
 }
 
+// A class that allows drawing a curved path with increasing width.
 class GradiantWidthPath {
-  constructor(time_to_bottom, note) {
+  constructor(time_to_bottom, note, note_to_color) {
     let end_point = this.getBottomByNote(note);
-    let color = this.getColorByNote(note);
+    let color = this.getColorByNote(note, note_to_color);
     console.log(end_point)
     var speed = view.size.getHeight() / 10;
     var hor_speed = 2;
@@ -61,6 +50,12 @@ class GradiantWidthPath {
 
     this.path.add(end_point);
     var current_point = end_point
+    /*
+    Draw the path from the bottom of the screen up. Each step of the loop adds a new point
+    to the path which is one tenth of the screen above the previous path, with a random
+    horizontal offset, increasing as we go up. Meaning, the path curves wider the further up
+    it is.
+    */
     do {
       var next = current_point - new Point(hor_speed * (Math.random() - 0.5), 1) * speed;
       if (next.x < view.bounds.left || next.x > view.bounds.right)
@@ -71,8 +66,8 @@ class GradiantWidthPath {
       current_point = next;
     } while (current_point.y >= view.bounds.top);
 
+    // We then smooth the path to make it curvy
     this.path.smooth();
-    console.log(this.path);
 
     this.closure = new Path();
     this.closure.fillColor = color;
@@ -84,6 +79,8 @@ class GradiantWidthPath {
     this.played = false;
   }
 
+  // Function that calclates the destination point of the path according to the
+  // musical note
   getBottomByNote(note) {
     let position_index = note_to_position[note.note_char];
     let bottom_point_x = (position_index + 0.5) / 7.0;
@@ -91,16 +88,20 @@ class GradiantWidthPath {
     return bottom_point;
   }
 
-  getColorByNote(note) {
+  // Fucntion gets the color of the path according to the musical note and the song's
+  // color scheme
+  getColorByNote(note, note_to_color) {
     let color = new Color(note_to_color[note.note_char]);
     color.brightness += (Math.random() - 0.5) * 0.1
     return color;
   }
 
+  // Returns true if the path is approching the end and is within the target bounds.
   isOnEnd() {
     return this.time_to_bottom * 0.8 < this.current_time && this.current_time < this.time_to_bottom;
   }
 
+  // Plays the note's sound according to the path's position
   playSound() {
     console.log(this.time_to_bottom, this.current_time)
     if (this.played)
@@ -115,14 +116,20 @@ class GradiantWidthPath {
     this.played = true;
   }
 
+  // Function in charge of continiously progressing the path from top of the screen
+  // to bottom. Returns true if the path reached the bottom of the screen.
   advanceClosure(delta) {
     var i = (this.current_time / this.time_to_bottom) * this.path.length;
     this.current_time += delta;
 
     var end = Math.min(this.path.length, (this.current_time / this.time_to_bottom) * this.path.length)
 
+    // The below code was inspired by paper.js's tutorial on using mouse vectors.
+    // The code creates a "skeleton" along the original path using the normal vector
+    // on various points. It then creates a closed polygon encompasing this skeleton.
     do {
       let location = this.path.getLocationAt(this.path.length - i);
+
       var step = (location.normal * 10 * (0.2 + i / this.path.length)) ;
       var top = location.point + step;
       var bottom = location.point - step;
@@ -174,11 +181,13 @@ class Note {
   }
 }
 
-
+// A class that represents a song, and yields notes according to the timing of the song.
 class Song {
-  constructor(rate, notes) {
+  constructor(rate, notes, note_to_color) {
     this.rate = rate;
     this.time_frames = [];
+    this.note_to_color = note_to_color;
+    console.log(this.note_to_color)
     console.log(notes);
     for (const n of notes) {
       if (n == null) {
@@ -194,10 +203,13 @@ class Song {
     this.on_end_callback = null
   }
 
+  // Registers a callback to be called on the song's end
   onEnd(callback) {
     this.on_end_callback = callback;
   }
 
+  // If it is time to play the next note, returns it.
+  // Otherwise, returns null.
   yieldNextNote(delta) {
     if (this.next_frame >= this.time_frames.length) {
       if (this.on_end_callback != null)
@@ -221,6 +233,7 @@ class Song {
   }
 }
 
+// This object allows state management of the app.
 const app_states = {
   START: "start",
   PLAY: "playing",
@@ -228,6 +241,7 @@ const app_states = {
   SHOW_PAINT: "showing painting"
 }
 
+// This class creates the game on the screen, and is in charge of the game mechanics.
 class AbstractPiano {
   constructor(song) {
     this.paths = [];
@@ -244,13 +258,14 @@ class AbstractPiano {
     this.createMarkers();
   }
 
+  // Function displays the target bounds at the bottom of the screen
   createMarkers() {
     this.text_markers = [];
     this.gradients = [];
 
     for (let note in note_to_key) {
       let key = note_to_key[note];
-      let note_color = note_to_color[note];
+      let note_color = this.song.note_to_color[note];
       let position_index = note_to_position[note];
 
       let bottom_point = new Point((position_index + 0.5) / 7.0, 1) * view.size;
@@ -291,7 +306,7 @@ class AbstractPiano {
   }
 
   addPath(bottom, note) {
-    var new_path = new GradiantWidthPath(3.0, note);
+    var new_path = new GradiantWidthPath(3.0, note, this.song.note_to_color);
     this.paths.push(new_path);
   }
 
@@ -301,6 +316,7 @@ class AbstractPiano {
     this.finished_paths.push(path);
   }
 
+  // Function advances all displayed paths.
   advancePaths(frame_event) {
     var remaining_paths = [];
     while (this.paths.length != 0) {
@@ -316,8 +332,6 @@ class AbstractPiano {
       this.state = app_states.END_PLAYING;
   }
 
-
-
   getNextNote(frame_event) {
     var next_note = this.song.yieldNextNote(frame_event.delta);
     if (next_note == null)
@@ -329,10 +343,55 @@ class AbstractPiano {
     this.addPath(end_point, next_note);
   }
 
+  // Presents final "masterpiece" of paths
   showAllPaths() {
     for (let path of this.finished_paths) {
       path.appear();
     }
+
+    // After 3 seconds, displays a button that navigates back to the homepage
+    setTimeout(() => {
+      const replay = new PointText({
+        position: view.center - [0, 100],
+        justification: 'center',
+        content: 'Play another Song',
+        fillColor: '#fff',
+        fontFamily: 'Times New Roman',
+        fontSize: 48,
+      });
+
+      var replayBtn = new Path.Rectangle(replay.bounds);
+      replayBtn.scale(1.3);
+      replayBtn.fillColor = '#000';
+      replayBtn.opacity = 1;
+      replayBtn.shadowColor = new Color(0, 0, 0);
+      replayBtn.shadowBlur = 10;
+      replayBtn.shadowOffset = new Point(1, 1);
+      replay.bringToFront();
+
+      replay.onMouseEnter = function (event) {
+        this.scale(1.1)
+      }
+
+      replay.onMouseLeave = function (event) {
+        this.scale(1 / 1.1)
+      }
+
+      replay.onClick = function (event) {
+        paper.project.activeLayer.removeChildren();
+        app = null;
+        showHomePage();
+      };
+
+      var group = new Group();
+
+      group.addChild(replayBtn);
+      group.addChild(replay);
+
+      group.opacity = 0;
+
+      group.tweenTo({ opacity: 1 }, 1000);
+    }, 3000);
   }
 
   removeAllPaths() {
@@ -367,6 +426,8 @@ class AbstractPiano {
       if (note_to_key[path.note.note_char] != event.character.toUpperCase())
         continue;
 
+      // When key is pressed on time, plays the note and presents a "clicking"
+      // animation
       if (path.isOnEnd()) {
         path.playSound();
         var position_index = note_to_position[path.note.note_char];
@@ -394,24 +455,23 @@ class AbstractPiano {
       if (note_to_key[note] != event.character.toUpperCase())
         continue;
 
-      wrong_piano.play(note, 4, 0.3 * 60 / this.song.rate);
+      wrong_piano.play(note, 3, 0.3 * 60 / this.song.rate);
     }
   }
 }
 
-var app = null;
-
 /* Call this fucntion with a the list of notes instance after you picked a song */
-const initializeApp = (rate, notes) => {
+const initializeApp = (rate, notes, note_to_color) => {
   const gamescreen = new Layer();
   gamescreen.activate();
   console.log(notes);
-  var song = new Song(rate, notes)
+  var song = new Song(rate, notes, note_to_color)
   app = new AbstractPiano(song);
 };
 
 const showHomePage = () => {
 
+  document.getElementById("logoImg").style.display = "none";
   document.getElementById("taulogoImg").style.display = "none";
   document.getElementById("githubImgGabi").style.display = "none";
   document.getElementById("githubImgLeora").style.display = "none";
@@ -421,11 +481,15 @@ const showHomePage = () => {
 
   //Greeting Text for the user
   
+  const logoImg = new Raster('logoImg');
+  logoImg.position = view.center - [0,200];
+  logoImg.justification = 'center'
+  logoImg.scale(0.25);
 
   const welcomeText = new PointText({
-    position: view.center + [0,-100],
+    position: logoImg.position + [0,150],
     justification: 'center',
-    content: 'Welcome to Abstract Piano',
+    content: 'Start Playing',
     fillColor: '#fff',
     fontFamily: 'Times New Roman',
     fontWeight: 'italic',
@@ -435,7 +499,7 @@ const showHomePage = () => {
     var welcomeBtn = new Path.Rectangle(welcomeText.bounds);
     welcomeBtn.scale(1.3)
     welcomeBtn.fillColor = '#000'
-    welcomeBtn.opacity = 0.5
+    welcomeBtn.opacity = 1
     welcomeBtn.shadowColor = new Color(0,0,0)
     welcomeBtn.shadowBlur = 10,
     welcomeBtn.shadowOffset = new Point(1, 1)
@@ -524,9 +588,6 @@ const showHomePage = () => {
       window.open("https://github.com/LeoraKowen");
     }
 
-
-    
-    
     //This function displays the song list and sets a new layer as active in PaperJs
     const showSongList = () => {
 
@@ -536,7 +597,7 @@ const showHomePage = () => {
 
         //These are the children of the active layer (songListLayer)
         const pickSongText = new PointText({
-          position: view.center + [0,-100],
+          position: view.center + [0,-200],
           justification: 'center',
           content: 'Pick a song:',
           fillColor: 'black',
@@ -544,13 +605,11 @@ const showHomePage = () => {
           fontWeight: 'italic',
           fontSize: 48
           });
- /********************************************************************************/
-       
 
         const song1 = new PointText({
           position: pickSongText.position + [0,100],
           justification: 'center',
-          content: 'Yonatan Hakatan',
+          content: 'Bach\'s Suite No.1',
           fillColor: '#fff',
           fontFamily: 'Times New Roman',
           fontSize: 24,
@@ -559,7 +618,7 @@ const showHomePage = () => {
         var song1Btn = new Path.Rectangle(song1.bounds);
         song1Btn.scale(1.3);
         song1Btn.fillColor = '#000'
-        song1Btn.opacity = 0.5
+        song1Btn.opacity = 1
         song1Btn.shadowColor = new Color(0,0,0)
         song1Btn.shadowBlur = 10,
         song1Btn.shadowOffset = new Point(1, 1)  
@@ -574,9 +633,6 @@ const showHomePage = () => {
               this.scale(1/1.1)
           }
 
-          /********************************************************************************/
-          
-
           const song2 = new PointText({
             position: song1.position + [0,100],
             justification: 'center',
@@ -589,7 +645,7 @@ const showHomePage = () => {
           var song2Btn = new Path.Rectangle(song2.bounds);
           song2Btn.scale(1.3);
           song2Btn.fillColor = '#000'
-          song2Btn.opacity = 0.5
+          song2Btn.opacity = 1
           song2Btn.shadowColor = new Color(0,0,0)
           song2Btn.shadowBlur = 10,
           song2Btn.shadowOffset = new Point(1, 1)  
@@ -603,40 +659,60 @@ const showHomePage = () => {
                 this.scale(1/1.1)
             }
 
-         
-
-          /********************************************************************************/
-
           const song3 = new PointText({
-          position: song2.position + [0,100],
-          justification: 'center',
-          content: 'Astronomia',
-          fillColor: '#fff',
-          fontFamily: 'Times New Roman',
-          fontSize: 24
+            position: song2.position + [0,100],
+            justification: 'center',
+            content: 'Astronomia',
+            fillColor: '#fff',
+            fontFamily: 'Times New Roman',
+            fontSize: 24
           });
       
           var song3Btn = new Path.Rectangle(song3.bounds);
           song3Btn.scale(1.3);
           song3Btn.fillColor = '#000'
-          song3Btn.opacity = 0.5
+          song3Btn.opacity = 1
           song3Btn.shadowColor = new Color(0,0,0)
           song3Btn.shadowBlur = 10,
           song3Btn.shadowOffset = new Point(1, 1)  
           song3.bringToFront()
 
-          song3.onMouseEnter = function(event) {
-            this.scale(1.1)
-          }
+            song3.onMouseEnter = function(event) {
+              this.scale(1.1)
+            }
+          
+            song3.onMouseLeave = function(event) {
+                this.scale(1/1.1)
+            }
+
+          const song4 = new PointText({
+            position: song3.position + [0,100],
+            justification: 'center',
+            content: 'Yonatan Hakatan',
+            fillColor: '#fff',
+            fontFamily: 'Times New Roman',
+            fontSize: 24
+            });
         
-          song3.onMouseLeave = function(event) {
-              this.scale(1/1.1)
-          }
-      
-          /********************************************************************************/
+            var song4Btn = new Path.Rectangle(song4.bounds);
+            song4Btn.scale(1.3);
+            song4Btn.fillColor = '#000'
+            song4Btn.opacity = 1
+            song4Btn.shadowColor = new Color(0,0,0)
+            song4Btn.shadowBlur = 10,
+            song4Btn.shadowOffset = new Point(1, 1)  
+            song4.bringToFront()
+  
+            song4.onMouseEnter = function(event) {
+              this.scale(1.1)
+            }
+          
+            song4.onMouseLeave = function(event) {
+                this.scale(1/1.1)
+            }
 
           const expText = new PointText({
-            position: song3.position + [0,100],
+            position: song4.position + [0,100],
             justification: 'center',
             content: 'To create your abstract masterpiece, try to match the flowing lines with the boxes below according to each letter on the keyboard',
             fillColor: 'black',
@@ -644,7 +720,8 @@ const showHomePage = () => {
             fontWeight: 'italic',
             fontSize: 20
             });
-
+          
+          //When wanting to add more songs, use the following code to do so
           /********************************************************************************/
 
           /*const songRandom = new PointText({
@@ -671,17 +748,22 @@ const showHomePage = () => {
     
             song1.onClick = function(event) {
               paper.project.activeLayer.removeChildren();
-              initializeApp(yonatan_song.rate, yonatan_song.notes);
+              initializeApp(bach_song.rate, bach_song.notes, bach_song.note_to_color);
             }
             
             song2.onClick = function(event) {
               paper.project.activeLayer.removeChildren();
-              initializeApp(jinglebells_song.rate, jinglebells_song.notes);
+              initializeApp(jinglebells_song.rate, jinglebells_song.notes, jinglebells_song.note_to_color);
             }
 
             song3.onClick = function(event) {
               paper.project.activeLayer.removeChildren();
-              initializeApp(astronomia_song.rate, astronomia_song.notes);
+              initializeApp(astronomia_song.rate, astronomia_song.notes, astronomia_song.note_to_color);
+            }
+
+            song4.onClick = function(event) {
+              paper.project.activeLayer.removeChildren();
+              initializeApp(yonatan_song.rate, yonatan_song.notes, yonatan_song.note_to_color);
             }
 }
 
--- leora_and_gabi_project/yonatan.js
+++ leora_and_gabi_project/yonatan.js
@@ -1,5 +1,21 @@
 const yonatan_song = {
-  "rate" : 60,
+  "rate": 90,
+  "note_to_color" : {
+    "C": "#20134d",
+    "D": "#76a5af",
+    "E": "#8988ad",
+    "F": "#d9a944",
+    "G": "#bc706b",
+    "A": "#8a2813",
+    "B": "#b45d1a",
+    "C#": "#20134d",
+    "D#": "#76a5af",
+    "E#": "#8988ad",
+    "F#": "#d9a944",
+    "G#": "#bc706b",
+    "A#": "#8a2813",
+    "B#": "#b45d1a"
+  },
   "notes" : [
     {
       "note": "G",
@@ -68,5 +84,199 @@ const yonatan_song = {
       "duration": 2, // length in beats
       "octave": 4
     },
+    null,
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "F",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "C",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "C",
+      "duration": 4, // length in beats
+      "octave": 4
+    },
+    null,
+    null,
+    null,
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "F",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "F",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "G",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "F",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "D",
+      "duration": 2, // length in beats
+      "octave": 4
+    },
+    null,
+    {
+      "note": "C",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "E",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "G",
+      "duration": 1, // length in beats
+      "octave": 4
+    },
+    {
+      "note": "C",
+      "duration": 4, // length in beats
+      "octave": 4
+    },
+    null,
+    null,
+    null,
+
   ]
 };
